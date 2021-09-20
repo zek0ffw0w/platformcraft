d0a8ae2a5c4f018cf544de0d88798a8a77dbcd47
zek0ffw0w <56290121+zek0ffw0w@users.noreply.github.com>
2021-09-20 17:05:43 +0300

    Add files via upload

---
+++ sandbox.py
@@ -0,0 +1,82 @@
+import platformcraft
+import logging
+import logging.config
+
+LOGIN = "zek0ffw0w"
+PASSWORD = "123456"
+
+LOCAL_PATH = "C:/Users/malyc/Downloads/wiegand-produkte_en_1-710x368.mp4"
+PC_PATH = "vidik"
+info_test = "file_info_test"
+params = {'name': "file_info_test", 'description': "test description", 'private': False}
+
+
+def main():
+    try:
+        session = platformcraft.Session(LOGIN, PASSWORD)
+    except Exception as e:
+        logging.error("unsuccessful login: %s", e)
+        return
+
+    filespot = session.filespot()
+
+    try:
+        object = filespot.upload(LOCAL_PATH, PC_PATH)
+    except Exception as e:
+        logging.error("unsuccessful upload: %s", e)
+        return
+
+    try:
+        object = filespot.remove(PC_PATH)
+    except Exception as e:
+        logging.error("unsuccessful remove: %s", e)
+        return
+
+    try:
+        object = filespot.file_info(info_test)
+    except Exception as e:
+        logging.error("unsuccessful getting info: %s", e)
+        return
+
+    try:
+        object = filespot.change(info_test, params)
+    except Exception as e:
+        logging.error("unsuccessful changing: %s", e)
+        return
+
+    try:
+        session = session.refresh()
+    except Exception as e:
+        logging.error("unsuccessful refresh: %s", e)
+        return
+
+
+if __name__ == '__main__':
+    DEFAULT_LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': '{asctime} {levelname}:{module}:{message}',
+                'style': '{',
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple',
+            },
+        },
+        'loggers': {
+            '': {
+                'level': 'DEBUG',
+            },
+            'platformcraft': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            },
+        }
+    }
+    logging.config.dictConfig(DEFAULT_LOGGING)
+
+    main()
