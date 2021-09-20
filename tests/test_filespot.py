81a1cdcab91d664a293458f712130d936e3a3114
zek0ffw0w <malychev1976@yandex.ru>
2021-09-20 16:53:26 +0300

    second commit

---
+++ tests/test_filespot.py
@@ -0,0 +1,58 @@
+from ..session import *
+
+import unittest
+from unittest.mock import patch, mock_open
+import json
+
+LOCAL_PATH = "C:/Users/malyc/Downloads/wiegand-produkte_en_1-710x368.mp4"
+PC_PATH_INFO_TEST = "file_info_test"
+PC_PATH_TEST = "upltest"
+name = {'description': "TESTTTTT"}
+
+LOGIN = "zek0ffw0w"
+PASSWORD = "123456"
+
+
+class TestFilespot(unittest.TestCase):
+    def setUp(self):
+        self.session = Session(LOGIN, PASSWORD)
+        self.filespot = self.session.filespot()
+        self.url = "https://filespot.platformcraft.ru/2/fs/container/" + self.session.owner_id + '/object/'
+
+    def test_upload(self):
+        with patch("builtins.open", mock_open(read_data="data")) as mock_file:
+            assert open(LOCAL_PATH).read() == "data"
+            mock_file.assert_called_with(LOCAL_PATH)
+
+    def test_remove(self):
+        result_delete = self.filespot.remove(PC_PATH_TEST)
+        # self.assertEqual(result_delete.status_code, 200)
+        # self.assertNotIn(PC_PATH_TEST, self.url)
+
+    def test_change(self):
+        url_put = "https://filespot.platformcraft.ru/2/fs/container/" + self.session.owner_id + '/object/' + PC_PATH_INFO_TEST
+
+        with patch('requests.post') as mock_post:
+            mock_post.return_value.status_code = 200
+            mock_post.return_value.json.return_value = name
+
+        result_put = self.session.put(url_put, data=json.dumps(name))
+        result_info = self.filespot.file_info(PC_PATH_INFO_TEST)
+        # self.assertIn(list(name)[0], result_info.text)
+        # self.assertEqual(result_put.status_code, 200)
+
+    def test_file_info(self):
+        check_in = "duration"
+
+        with patch('requests.post') as mock_post:
+            mock_post.return_value.status_code = 200
+            mock_post.return_value.json.return_value = check_in
+
+            filespot = self.session.filespot()
+            response = filespot.file_info(PC_PATH_INFO_TEST)
+        # self.assertIn(check_in, response.text)
+        # self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_session.py
@@ -0,0 +1,56 @@
+from ..session import *
+from ..auth import *
+
+import unittest
+from mock import patch
+
+
+login = "zek0ffw0w"
+password = "123456"
+url = "https://platformcraft.ru/"
+
+
+class TestSession(unittest.TestCase):
+    def setUp(self):
+        self.session = Session(login, password)
+        self.name = {'name': "test_7", 'description': "test description", 'private': False}
+
+    def test_post(self):
+        with patch('session.requests.post') as mock_post:
+            mock_post.return_value.status_code = 200
+
+        response = self.session.post(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get(self):
+        with patch('session.requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+
+        response = self.session.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_put(self):
+        pc_path = "test_6"
+        url_put = "https://filespot.platformcraft.ru/2/fs/container/" + self.session.owner_id + '/object/' + pc_path
+
+        result_put = self.session.put(url_put, data=json.dumps(self.name))
+        self.assertEqual(result_put.status_code, 200)
+
+        #     mock_put.return_value.status_code = 200
+        #
+        # response = self.session.put(url_put)
+        #
+        # self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.json(), fake_json)
+
+    def test_delete(self):
+        pc_path = "test_7"
+        url_delete = "https://filespot.platformcraft.ru/2/fs/container/" + self.session.owner_id + '/object/' + pc_path
+
+        result_delete = self.session.delete(url_delete)
+        self.assertEqual(result_delete.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
