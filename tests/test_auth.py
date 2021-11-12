5db9939cad7c439e366002eeff5cc1a9a5877f24
zek0ffw0w <malychev1976@yandex.ru>
2021-11-12 17:07:00 +0300

    completed test for auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,6 +8,7 @@ password = "123456"
 
 fake_auth_json = {'user_id': "uid", 'owner_id': "oid", 'access_token': "acces_tok", 'expires_at': 1, 'refresh_token': "ref_tok"}
 
+
 class TestAuth(unittest.TestCase):
 
     def setUp(self):
@@ -22,7 +23,6 @@ class TestAuth(unittest.TestCase):
         self.assertEqual(auth_info.user_id, fake_auth_json["user_id"])
 
     def test_token_forbidden(self):
-
         with patch('platformcraft.auth.requests.post') as mock_post:
             mock_post.return_value.status_code = 403
             mock_post.return_value.ok = False
@@ -31,17 +31,95 @@ class TestAuth(unittest.TestCase):
             with self.assertRaises(ExceptionForbidden) as context:
                 auth_info = self.auth.token("zek0ffw0w", "123456")
 
-    def test_refresh(self):
+    def test_token_bad_request(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 400
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionBadRequest) as context:
+                auth_info = self.auth.token("zek0ffw0w", "123456")
+
+    def test_token_not_found(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 404
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionNotFound) as context:
+                auth_info = self.auth.token("zek0ffw0w", "123456")
+
+    def test_token_conflict(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 409
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionConflict) as context:
+                auth_info = self.auth.token("zek0ffw0w", "123456")
+
+    def test_token_too_many_requests(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 429
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionTooManyRequests) as context:
+                auth_info = self.auth.token("zek0ffw0w", "123456")
+
+    def test_refresh_ok(self):
         with patch('platformcraft.auth.requests.post') as mock_post:
             mock_post.return_value.status_code = 200
             mock_post.return_value.json.return_value = fake_auth_json
+            auth_info = self.auth.refresh()
 
-            response = self.auth.refresh()
+        self.assertEqual(auth_info.user_id, fake_auth_json["user_id"])
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), fake_auth_json)
+    def test_refresh_forbidden(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 403
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionForbidden) as context:
+                auth_info = self.auth.refresh()
+
+    def test_refresh_bad_request(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 400
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionBadRequest) as context:
+                auth_info = self.auth.refresh()
+
+    def test_refresh_not_found(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 404
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionNotFound) as context:
+                auth_info = self.auth.refresh()
+
+    def test_refresh_conflict(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 409
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionConflict) as context:
+                auth_info = self.auth.refresh()
+
+    def test_refresh_too_many_requests(self):
+        with patch('platformcraft.auth.requests.post') as mock_post:
+            mock_post.return_value.status_code = 429
+            mock_post.return_value.ok = False
+            mock_post.return_value.json.return_value = fake_auth_json
+
+            with self.assertRaises(ExceptionTooManyRequests) as context:
+                auth_info = self.auth.refresh()
 
 
 if __name__ == '__main__':
-
     unittest.main()
