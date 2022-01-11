2e76da2b5103e86612246c018c24f02979c8f553
zek0ffw0w <malychev1976@yandex.ru>
2022-01-11 20:54:31 +0300

    added deleting slash method, dir func

--- platformcraft/filespot.py
+++ platformcraft/filespot.py
@@ -10,27 +10,29 @@ class Filespot():
     def __init__(self, Session):
         self.session = Session
 
-    def upload(self, local_path, pc_path):
+    def upload(self, local_path, pc_path, is_dir):
         logger.debug("filespot.upload: %s as: %s", local_path, pc_path)
 
-        if pc_path[0] == "/":
-            pc_path = pc_path[1:]
-
+        pc_path = self._del_slash(pc_path)
         url = FILESPOT_ADDR + self.session.owner_id + '/object/' + pc_path
 
-        try:
-            f = open(local_path, 'rb')
-        except Exception as e:
-            raise ExceptionOpenFile("cant open file: {}".format(e)) from None
+        # bad request???
+        if is_dir:
+            response = self.session.post(url + '/')
+        else:
+            try:
+                f = open(local_path, 'rb')
+            except Exception as e:
+                raise ExceptionOpenFile("cant open file: {}".format(e)) from None
 
-        with f:
-            response = self.session.post(url, files={'file': f})
+            with f:
+                response = self.session.post(url, files={'file': f})
+                print(response.text)
 
     def remove(self, pc_path):
         logger.debug("filespot.remove: %s", pc_path)
 
-        if pc_path[0] == "/":
-            pc_path = pc_path[1:]
+        pc_path = self._del_slash(pc_path)
 
         url = FILESPOT_ADDR + self.session.owner_id + '/object/' + pc_path
 
@@ -39,8 +41,7 @@ class Filespot():
     def change(self, pc_path, params):
         logger.debug("filespot.change: %s", pc_path)
 
-        if pc_path[0] == "/":
-            pc_path = pc_path[1:]
+        pc_path = self._del_slash(pc_path)
 
         url = FILESPOT_ADDR + self.session.owner_id + '/object/' + pc_path
 
@@ -52,10 +53,14 @@ class Filespot():
     def file_info(self, pc_path):
         logger.debug("filespot.file_info: %s", pc_path)
 
-        if pc_path[0] == "/":
-            pc_path = pc_path[1:]
+        pc_path = self._del_slash(pc_path)
 
         url = FILESPOT_ADDR + self.session.owner_id + '/object/' + pc_path
 
         response = self.session.get(url)
         return response
+
+    def _del_slash(self, path):
+        if path[0] == "/":
+            path = path[1:]
+        return path
