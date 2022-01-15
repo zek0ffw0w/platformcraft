d78ba7dacda3ae5f5667e3d0c1eafe7245c4d3b8
zek0ffw0w <malychev1976@yandex.ru>
2022-01-16 01:59:34 +0300

    completed tests

--- platformcraft/tests/test_filespot.py
+++ platformcraft/tests/test_filespot.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 LOCAL_PATH = "platformcraft/tests/test_data/test.jpg"
 LOCAL_PATH_FAKE = "fake_path"
-PC_PATH_TEST1 = "test21"
+PC_PATH_TEST1 = "test213"
 PC_PATH_TEST2 = "test22"
 PC_PATH_TEST3 = "c333"
 FILESPOT_ADDR = "https://filespot.platformcraft.ru/2/fs/container/"
@@ -20,48 +20,32 @@ class TestFilespot(unittest.TestCase):
     @patch('platformcraft.filespot')
     def test_change(self, MockChange):
         filespot = MockChange()
-        filespot.change.return_value.status_code = 404
-        filespot.change.return_value.ok = False
+        filespot.change(PC_PATH_TEST1, name=PC_PATH_TEST1, description="33", private=False)
 
+    @patch('platformcraft.filespot')
+    def test_file_info(self, MockInfo):
+        filespot = MockInfo()
+        filespot.file_info(PC_PATH_TEST1)
+
+    @patch('platformcraft.filespot')
+    def test_remove(self, MockRemove):
+        filespot = MockRemove()
+        filespot.remove(PC_PATH_TEST1)
+
+    @patch('platformcraft.filespot')
+    def test_upload(self, MockUpload):
+        filespot = MockUpload()
+        filespot.upload(PC_PATH_TEST1, PC_PATH_TEST2)
+
+    def test_exception_open_file(self):
+        with self.assertRaises(ExceptionOpenFile) as context:
+            self.filespot = self.session.filespot()
+            self.filespot.upload(LOCAL_PATH_FAKE, PC_PATH_TEST1, is_dir=False)
+
+    def test_exception_server(self):
         with self.assertRaises(ExceptionNotFound) as context:
-            resp = filespot.change(PC_PATH_TEST3, description="43244")
-            print(resp.status_code)
-
-
-    # def test_file_info(self):
-    #     # check normal behaviour
-    #     with patch('platformcraft.filespot') as mock_file_info:
-    #         mock_file_info.return_value.status_code = 200
-    #         mock_file_info.return_value.ok = False
-    #
-    #         with self.assertRaises(ExceptionNotFound) as context:
-    #             self.filespot = self.session.filespot()
-    #             auth_info = self.filespot.file_info(PC_PATH_TEST2)
-    #
-    # def test_remove(self):
-    #     # check normal behaviour
-    #     self.filespot = self.session.filespot()
-    #     response = self.filespot.remove(PC_PATH_TEST2)
-    #
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_upload(self):
-    #     # check normal behaviour
-    #     self.filespot = self.session.filespot()
-    #     response = self.filespot.upload(LOCAL_PATH, PC_PATH_TEST1)
-    #
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_exception_open_file(self):
-    #     with self.assertRaises(ExceptionOpenFile) as context:
-    #         self.filespot = self.session.filespot()
-    #         self.filespot.upload(LOCAL_PATH_FAKE, PC_PATH_TEST1, is_dir=False)
-    #
-    # def test_exception_server(self):
-    #     with self.assertRaises(ExceptionNotFound) as context:
-    #         self.filespot = self.session.filespot()
-    #         params = {'name': "file_info_test", 'description': "test description", 'private': False}
-    #         self.filespot.change(PC_PATH_TEST1, params)
+            self.filespot = self.session.filespot()
+            self.filespot.change(PC_PATH_TEST1, name=PC_PATH_TEST1, description="33", private=False)
 
 
 if __name__ == '__main__':
